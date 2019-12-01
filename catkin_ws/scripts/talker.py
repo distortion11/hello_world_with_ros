4349c8c6d44c8774f2a650c900d87884992435ee
Sasha <sashagrigorev920@gmail.com>
2019-12-01 20:00:40 +0300

    second commit

--- catkin_ws/scripts/talker.py
+++ catkin_ws/scripts/talker.py
@@ -42,9 +42,9 @@ from std_msgs.msg import String
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(1) #10  HZ
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+       	hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
